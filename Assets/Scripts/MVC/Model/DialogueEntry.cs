2372cd37ddbede83916bf15aabead0dd204e1f27
acced <1415351793@qq.com>
2024-09-23 17:57:29 +0800

    Model

---
+++ Assets/Scripts/MVC/Model/DialogueEntry.cs
@@ -0,0 +1,13 @@
+
+
+namespace DialogueSystem
+{
+    [System.Serializable]
+    public class DialogueEntry
+    {
+        /// <summary>
+        /// The dialogue entry ID. Links reference dialogue entries by ID.
+        /// </summary>
+        public int id = 0;
+    }
+}
