387bf170a8a6427c8adf3597b6500c6157fd29a5
acced <1415351793@qq.com>
2024-11-24 12:58:35 +0800

    提交

--- Assets/Editor/DialogueGraph/DialogueEditorWindow.cs
+++ Assets/Editor/DialogueGraph/DialogueEditorWindow.cs
@@ -8,7 +8,7 @@ namespace Editor.DialogueGraph
 {
     public  class DialogueEditorWindow : EditorWindow
     {
-        private Font m_customFont;
+       
         
         [MenuItem("Window/DG/Dialogue NodeGraph")]
         public static void Open()
@@ -22,7 +22,6 @@ namespace Editor.DialogueGraph
 
             AddStyles();
             
-            m_customFont = AssetDatabase.LoadAssetAtPath<Font>("Assets/Editor Default Resources/Dialogue Graph/HYBlackMythTrailblazer.ttf");
         }
 
         private void AddStyles()
